0f18946d734e7490ae8927e2802bf2ba67dcb91f
Tim Akinbo <takinbo@gmail.com>
2010-04-28 02:58:27 +0100

    added autoslugs to the Post model.
    Also added unicode representation for the models
    and a few field renaming.

--- orombo/webapp/models.py
+++ orombo/webapp/models.py
@@ -1,24 +1,34 @@
 # vim: ai sts=4 ts=4 et sw=4
 from django.db import models
 from django.contrib.auth.models import User
+from django_extensions.db.fields import AutoSlugField
 
 class Post(models.Model):
     '''The post model stores posts that are submitted on the site.'''
     title = models.CharField(max_length=255, help_text="The title for the post")
     url = models.URLField(verify_exists=True, help_text="A valid url to the resource")
-    description = models.CharField(max_length=255, help_text="A description for the post")
+    description = models.TextField(blank=True, null=True, help_text="A description for the post")
     author = models.ForeignKey(User, help_text="The user that authored the post")
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    slug = AutoSlugField(populate_from='title')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return u"%s" % (self.title)
 
 class Vote(models.Model):
     post = models.ForeignKey(Post)
     voter = models.ForeignKey(User)
     points = models.IntegerField()
 
+    def __unicode__(self):
+        return u'%s (%d)' % (self.post.title, self.points)
+
 class Comment(models.Model):
     post = models.ForeignKey(Post)
     author = models.ForeignKey(User)
-    comment = models.CharField(max_length=5000, help_text="The comment not exceeding 5000 characters")
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    comment = models.TextField(help_text="A user comment for the post")
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return u'%s' % (self.comment)
